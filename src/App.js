79fd7b01778aee539decfd73fbaa85f317041972
sujonphero <sujonsheikh.in@gmail.com>
2022-06-25 09:40:18 -0500

    project finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import FirstImage from './Components/FirstImage';
-import SecondImage from './Components/SecondImage';
-import ThirdImage from './Components/ThirdImage';
+import { Parallax } from 'react-parallax';
+import first from './asset/one.jpg'
+import second from './asset/two.jpg'
+import third from './asset/three.jpg'
 
 function App() {
   return (
-    <div>
-    <FirstImage/>
-    <SecondImage/>
-    <ThirdImage/>
+    <div className='w-full h-screen'>
+     <Parallax className='relative h-screen' bgImage={first} strength={200}>
+        <div className='absolute flex justify-center items-center w-full h-screen'>
+            <p className='bg-slate-200 uppercase font-medium p-2 px-4 rounded-md'>hello world</p>
+        </div>
+    </Parallax>
+
+
+    <Parallax className='relative h-screen' bgImage={second} blur={{ min: -15, max: 15 }} strength={300}>
+        <div className='absolute flex justify-center items-center w-full h-screen'>
+            <p className='bg-slate-200 uppercase font-medium p-2 px-4 rounded-md'>hello world</p>
+        </div>
+    </Parallax>
+
+
+    <Parallax className='relative h-screen' bgImage={third} strength={200}>
+        <div className='absolute flex justify-center items-center w-full h-screen'>
+            <p className='bg-slate-200 uppercase font-medium p-2 px-4 rounded-md'>hello world</p>
+        </div>
+    </Parallax>
     </div>
   );
 }
