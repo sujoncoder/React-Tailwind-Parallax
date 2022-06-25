72cf1f1baa725261393696607dc410293a39d3e2
sujonphero <sujonsheikh.in@gmail.com>
2022-06-25 05:58:46 -0500

    start simple parallax project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FirstImage from './Components/FirstImage';
+import SecondImage from './Components/SecondImage';
+import ThirdImage from './Components/ThirdImage';
+
+function App() {
+  return (
+    <div>
+    <FirstImage/>
+    <SecondImage/>
+    <ThirdImage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SecondImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SecondImage = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default SecondImage
